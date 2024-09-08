d7a35439ac04e414cb5aaabce6d9bdf09d20b38e
Ali Khan <alik@robarts.ca>
2024-09-08 01:40:58 -0400

    WIP: fixing qc so flatfieldqc isn't enabled with prestitched
    - while I am at it, will change jinja rules to maybe use snakemake
    built-in jinja

--- workflow/scripts/generate_subject_qc.py
+++ workflow/scripts/generate_subject_qc.py
@@ -8,9 +8,10 @@ env = Environment(loader=file_loader)
 template = env.get_template(snakemake.input.subject_html)
 
 # input html files
-ws_html = snakemake.input.ws_html
-ff_html = snakemake.input.ff_html
-vol_html = snakemake.input.vol_html
+in_htmls = snakemake.input.in_htmls
+#ws_html = snakemake.input.ws_html
+#ff_html = snakemake.input.ff_html
+#vol_html = snakemake.input.vol_html
 
 # output html files
 sub_html = snakemake.output.sub_html
@@ -22,13 +23,12 @@ acq = snakemake.wildcards.acq
 
 # Get relative path to the subjects QC htmls
 
-ws_rel_path = Path(ws_html).relative_to(Path(sub_html).parent)
-ff_rel_path = Path(ff_html).relative_to(Path(sub_html).parent)
-vol_rel_path = Path(vol_html).relative_to(Path(sub_html).parent)
+link_rel_paths = [ Path(link_path).relative_to(Path(sub_html).parent) for link_path in snakemake.input.link_paths]
+link_descriptions = snakemake.params.link_descriptions
 
 # Fill in jinja template for subject html and write it out
 output = template.render(back_link="../qc_report.html",subject=subject,sample=sample,acq=acq,
-                         ffhtml=ff_rel_path,wshtml=ws_rel_path, volhtml=vol_rel_path)
+                         link_rel_paths, link_descriptions)
 with open(sub_html, 'w') as f:
     f.write(output)
 
