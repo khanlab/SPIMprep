590cf78225be5abfcceece7251733baea4fac358
Ali Khan <alik@robarts.ca>
2025-03-04 09:08:23 -0500

    WIP: add uncommitted updates

--- dask-stitch/scripts/fuse_volume.py
+++ dask-stitch/scripts/fuse_volume.py
@@ -12,14 +12,14 @@ rtree_idx, znimgs = build_rtree_index(ome_zarr_paths)
 
 
 # Parameters for the fused array
-output_shape=(400,400,400)
-output_chunks=(100,100,100)
+output_shape=snakemake.params.output_shape
+output_chunks=snakemake.params.output_chunks
 fused_dask_array = da.zeros(output_shape, chunks=output_chunks, dtype=np.float32)
 znimg_out = ZarrNii.from_darr(fused_dask_array,
                                              axes_order=znimgs[0].axes_order,
                                              orientation=znimgs[0].get_orientation(),
-                                             spacing=(1,1,1),
-                                             origin=(-100,-100,-100))
+                                             spacing=snakemake.params.output_spacing,
+                                             origin=snakemake.params.output_origin)
 
 znimg_fused = fuse_output_blocks(znimg_out,rtree_idx=rtree_idx,tile_znimgs=znimgs)
 
