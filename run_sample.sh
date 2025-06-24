2da3b035e6ded693bfc6f272d667a9f7702a2fb9
Ali R. Khan <alik@robarts.ca>
2025-06-24 16:17:32 -0400

    create batch-submission script
    - uses samples.tsv to submit jobs with CLI
    - click and rich to make pretty

---
+++ run_sample.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --nodes=1
+
+eval "$(pixi shell-hook bash)"
+
+# Optionally print some env info
+echo "Environment activated with pixi."
+echo "Python version: $(python --version)"
+echo "Using RUN_PY_PATH: $RUN_PY_PATH"
+
+"$RUN_PY_PATH" \
+  --output-bids-dir $OUTPUT_BIDS_DIR \
+  --work-dir $WORK_DIR \
+  --stains $STAINS \
+  --subject $SUBJECT \
+  --acq $ACQ \
+  --input-path $INPUT_PATH \
+  -c all \
+  --use-conda \
+  --conda-prefix $CONDA_PREFIX
+
+
