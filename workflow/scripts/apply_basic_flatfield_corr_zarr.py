6dc0bfc917c4f8d85df0ab8024ed745abfb8acc5
Ali Khan <alik@robarts.ca>
2025-10-31 13:58:16 -0400

    trying out dask-distributed

--- workflow/scripts/apply_basic_flatfield_corr_zarr.py
+++ workflow/scripts/apply_basic_flatfield_corr_zarr.py
@@ -1,56 +1,70 @@
-import json
-from pathlib import Path
+if __name__ == "__main__":
 
-import dask.array as da
-import numpy as np
-from basicpy import BaSiC
-from dask.diagnostics import ProgressBar
-from skimage.transform import resize
+    from dask.distributed import Client, LocalCluster
 
-in_zarr = snakemake.input.zarr
+    cluster = LocalCluster(
+        n_workers=int(snakemake.threads / 2),  # or 32, depending on workload
+        threads_per_worker=2,  # isolate GIL
+        memory_limit="auto",  # or tune to your RAM
+        dashboard_address=":8788",
+    )
+    client = Client(cluster)
+    print(cluster.dashboard_link)
 
-# load data
-arr = da.from_zarr(in_zarr)
 
-img_shape = (arr.shape[3], arr.shape[4])
 
-chan_arr_list = []
+    import json
+    from pathlib import Path
 
-# loops over channels (one model for each channel):
-for i_channel, model in enumerate(snakemake.input.model_dirs):
+    import dask.array as da
+    import numpy as np
+    from basicpy import BaSiC
+    from dask.diagnostics import ProgressBar
+    from skimage.transform import resize
 
-    # load model (doing it here instead of using load_model function as it was buggy-- fix that later)..
-    model_path = Path(model)
-    with open(model_path / "settings.json") as fp:
-        model = json.load(fp)
+    in_zarr = snakemake.input.zarr
 
-    profiles = np.load(model_path / "profiles.npz")
-    model["flatfield"] = profiles["flatfield"]
-    model["darkfield"] = profiles["darkfield"]
+    # load data
+    arr = da.from_zarr(in_zarr)
 
-    basic = BaSiC()
-    basic.flatfield = resize(model["flatfield"], img_shape, preserve_range=True)
-    basic.darkfield = resize(model["darkfield"], img_shape, preserve_range=True)
-    basic.baseline = 0
+    img_shape = (arr.shape[3], arr.shape[4])
 
-    # select the channel first
-    arr_chan = arr[:, i_channel, :, :, :]
+    chan_arr_list = []
 
-    # now we want to apply correction to all images
-    # define a function to map
-    def apply_basic_parallel(x):
-        return np.reshape(
-            basic.transform(x.squeeze()), (1, 1, img_shape[0], img_shape[1])
-        ).astype("uint16")
+    # loops over channels (one model for each channel):
+    for i_channel, model in enumerate(snakemake.input.model_dirs):
 
-    arr_corr = da.map_blocks(apply_basic_parallel, arr_chan)
+        # load model (doing it here instead of using load_model function as it was buggy-- fix that later)..
+        model_path = Path(model)
+        with open(model_path / "settings.json") as fp:
+            model = json.load(fp)
 
-    chan_arr_list.append(arr_corr)
+        profiles = np.load(model_path / "profiles.npz")
+        model["flatfield"] = profiles["flatfield"]
+        model["darkfield"] = profiles["darkfield"]
 
-# stack along chans
-arr_stacked = da.stack(chan_arr_list, axis=1).rechunk(
-    [1, 1] + snakemake.params.out_chunks
-)
+        basic = BaSiC()
+        basic.flatfield = resize(model["flatfield"], img_shape, preserve_range=True)
+        basic.darkfield = resize(model["darkfield"], img_shape, preserve_range=True)
+        basic.baseline = 0
 
-with ProgressBar():
+        # select the channel first
+        arr_chan = arr[:, i_channel, :, :, :]
+
+        # now we want to apply correction to all images
+        # define a function to map
+        def apply_basic_parallel(x):
+            return np.reshape(
+                basic.transform(x.squeeze()), (1, 1, img_shape[0], img_shape[1])
+            ).astype("uint16")
+
+        arr_corr = da.map_blocks(apply_basic_parallel, arr_chan)
+
+        chan_arr_list.append(arr_corr)
+
+    # stack along chans
+    arr_stacked = da.stack(chan_arr_list, axis=1).rechunk(
+        [1, 1] + snakemake.params.out_chunks
+    )
+
     da.to_zarr(arr_stacked, snakemake.output.zarr, overwrite=True, zarr_format=2)
